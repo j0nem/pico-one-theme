3777372199c1574ee734b11327b7bbc88e9baece
Johannes Cram <johannes@jonesmedia.de>
2018-04-15 17:17:31 +0200

    Add support for p01-contact form

--- js/animations.js
+++ js/animations.js
@@ -1,34 +1,39 @@
-// Smooth scrolling with focus management
-// source: https://css-tricks.com/smooth-scrolling-accessibility/
-
-$('a[href*="#"]').not('[href="#"]').not('[href*="#img"]').click(function(e) {
-    target = $(this.hash);
-    if (target.length) {
-        e.preventDefault();
-        $('html, body').animate({
-            scrollTop: target.offset().top
-        }, 500, function() {
-            var $target = $(target);
-            $target.focus();
-            if ($target.is(":focus")) { // check if focussed
-                return false;
-              } else {
-                $target.attr('tabindex','-1'); // adding tabindex if not focussable
-                $target.focus(); // try again
-              };
-        });
-        //Change URL
-        window.location.hash = target.attr('id');
-    }
-});
-
 $(document).ready(function(){
     setMenuMode();
+    smoothAnchorScroll();
+    updateContactForm();
 });
 
 $(window).scroll(function(e) {
     setMenuMode();
 });
+
+/** 
+ * Smooth scrolling with focus management
+ * source: https://css-tricks.com/smooth-scrolling-accessibility/
+ */
+function smoothAnchorScroll() {
+    $('a[href*="#"]').not('[href="#"]').not('[href*="#lb-"]').click(function(e) {
+        target = $(this.hash);
+        if (target.length) {
+            e.preventDefault();
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 500, function() {
+                var $target = $(target);
+                $target.focus();
+                if ($target.is(":focus")) { // check if focussed
+                    return false;
+                } else {
+                    $target.attr('tabindex','-1'); // adding tabindex if not focussable
+                    $target.focus(); // try again
+                };
+            });
+            //Change URL
+            window.location.hash = target.attr('id');
+        }
+    });
+}
 
 function setMenuMode() {
     if($(window).scrollTop() > 300) {
@@ -37,7 +42,6 @@ function setMenuMode() {
         $('header').removeClass('small');
     }
     //mark the section menu, which is currently mainly in viewport
-    console.clear();
     $('header nav a').each(function(i) {
         section = $(this.hash);
         
@@ -53,3 +57,31 @@ function setMenuMode() {
         }
     });
 }
+
+function updateContactForm() {
+    $('#p01-contact1').submit(function(e){
+        e.preventDefault();
+        var form = $(this);
+        var post_url = form.attr('action');
+        var post_data = form.serialize();
+        var button = $(this).find('input[type=submit]');
+        button.val('Wird gesendet...');
+        button.attr('disabled','disabled');
+        $.ajax({
+            type: 'POST',
+            url: post_url,
+            dataType: 'html',
+            data: post_data,
+            success: function(msg) {
+                var result = $('<div />').append(msg).find('#p01-contact1').html();
+                $('#p01-contact1').html(result);
+                $.each(post_data.split('&'), function (index, elem) {
+                    var vals = elem.split('=');
+                    $("[name='" + decodeURIComponent(vals[0]) + "']").val(decodeURIComponent(vals[1].replace(/\+/g, ' ')));
+                    console.log(vals);
+                });
+            }
+        });
+    });
+}
+
